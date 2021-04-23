a7973b863f1c56031f42d68aefc63e76c4768b01
bhagyashrinalawade <bhagyashrinalawade96@gmail.com>
2021-04-23 18:58:37 +0530

    added student class

--- Library/settings.py
+++ Library/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("hello we are settings.py")
--- Library/urls.py
+++ Library/urls.py
@@ -32,3 +32,7 @@ urlpatterns = [
 dfgdfgdf
 dfgdfg
 dfgdfgdf
+cxvxcv
+xcvxcv
+
+cxvxcv
